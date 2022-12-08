5cde9c71618f86f2765ffc6a8a0e7504ced7647a
Williams Nimi <williamson.nimi@gmail.com>
2022-12-08 07:53:33 -0800

    fixed betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
-/*
- * This is a c file that prints the text
- * Programming is ike building a multilingual puzzle
- * It uses the puts function instead of printf.
- * the return value is 0
- */
 #include <stdio.h>
 #include <string.h>
-
 /**
- * This is the main function
+ * main - This is the main function.
+ *
+ * Description: This program prints out text using the put function.
+ *
+ * Return: 0
  */
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle";
+	char str1[] = "Programming is like building a multilingual puzzle\n";
 	puts(str1);
-
 	return (0);
 }
