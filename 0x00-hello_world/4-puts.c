5b906226bb1280d78195345f7d714f6da4b3caa2
Williams Nimi <williamson.nimi@gmail.com>
2022-12-08 07:12:16 -0800

    Created a c file to print characters onto the screen using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * This is a c file that prints the text
+ * Programming is ike building a multilingual puzzle
+ * It uses the puts function instead of printf.
+ * the return value is 0
+ */
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * This is the main function
+ */
+int main(void)
+{
+	char str1[] = "Programming is like building a multilingual puzzle";
+	puts(str1);
+
+	return (0);
+}
