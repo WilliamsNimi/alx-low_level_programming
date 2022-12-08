aa741a51fcca1856ff9135722e85d1433eddf556
Williams Nimi <williamson.nimi@gmail.com>
2022-12-08 08:29:05 -0800

    Created a C program that prints to stderror

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Description: This function prints to STDERR
+ * Returns: 1
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
