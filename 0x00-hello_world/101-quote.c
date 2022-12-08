2a0fb700462435034c1c54bc84a70c3a5982ccea
Williams Nimi <williamson.nimi@gmail.com>
2022-12-08 08:33:26 -0800

    Added the missing argument to fprintf. Not sure why betty cannot see my returns

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  * main - this is the main function
  *
  * Description: This function prints to STDERR
- * Returns: 1
+ * Returns: 1 (returns to standard error)
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
