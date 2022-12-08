13b2b93f94f66aebd30df5e7401c2be8e4f90a00
Williams Nimi <williamson.nimi@gmail.com>
2022-12-08 08:06:33 -0800

    Created a c file to print with printf. Strangely enough, betty passed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Thisis the main function
+ *
+ * Description: This function prints out value with printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
