a328f8100c86726b7276ba26bff8459992e93804
Williams Nimi <williamson.nimi@gmail.com>
2022-12-08 07:59:28 -0800

    fixed betty errors but betty still not happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle\n";
+	char str1[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(str1);
 	return (0);
 }
