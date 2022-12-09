f61b48aaec82bfe3a6a1a1dab930e0e677ff0d73
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 05:10:34 -0800

    More conditionals

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,10 +10,9 @@
 int main(void)
 {
 	int n;
-
+	int mod;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int mod;
 	mod = n % 10;
 	if (mod > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, mod);
