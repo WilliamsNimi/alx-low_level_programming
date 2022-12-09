c862dd3d0b17d0ac40b26dac77032e28a44fb6d9
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 05:04:58 -0800

    More conditionals

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- THis is the main function
+ *
+ * Description: This program checks and compares a number to others
+ * Return: This program returns zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int mod = n % 10;
+	if (mod > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, mod);
+	else if (mod < 6 && mod != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, mod);
+	else if (mod == 0)
+		printf("Last digit of %d is %d and is 0\n", n, mod);
+	return (0);
+}
