742eaf506e31fefb6986f7139206fe41b86dd6e9
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 08:48:36 -0800

    Printing numbers with put char and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- This is the main function
+ *
+ * Description: This function prints numbers with commas
+ * Return: it returns 0
+ */
+int main(void)
+{
+	int num;
+	for (num = 0, num <= 9; num++)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
