6dee6ad9bd2d41ddaff47b94d008256f6d5ae706
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 08:53:58 -0800

    Removed a trailing comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,16 @@ int main(void)
 	int num;
 	for (num = 0; num <= 9; num++)
 	{
-		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if(num == 9)
+		{
+			putchar(num + '0');
+		}
+		else
+		{
+			putchar(num + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
