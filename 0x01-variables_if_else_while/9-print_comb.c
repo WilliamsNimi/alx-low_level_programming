a97fca4c564af7883a3d8f7f89a83f592ac06d9c
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 11:29:14 -0800

    Fixed the code to print the correct output

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,12 @@
 int main(void)
 {
 	int num;
+
 	for (num = 0; num <= 9; num++)
 	{
-		if(num == 9)
+		putchar(num + '0');
+		if (num < 9)
 		{
-			putchar(num + '0');
-		}
-		else
-		{
-			putchar(num + '0');
 			putchar(',');
 			putchar(' ');
 		}
