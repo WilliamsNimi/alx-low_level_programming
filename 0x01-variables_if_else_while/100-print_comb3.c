eae03987177196600f2297b2876dbd9a5b278df7
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 11:40:53 -0800

    Smallest combination of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio>
+/**
+ * main- This is the main function
+ *
+ * Description: This functions prints a pattern of numbers
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 8 && b != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
