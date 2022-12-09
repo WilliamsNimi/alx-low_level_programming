96b2cdb048f9d1ac212a8f0f2b04fd401f886457
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 05:21:43 -0800

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- This is the main function
+ *
+ * Description: This function prints alphabets a-z
+ * Return: returns 0
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z' alpha++)
+		putchar(alpha);
+	return (0);
+}
