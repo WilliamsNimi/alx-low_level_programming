edb25976ebd5ed0603677440c267bf014b6f6c9a
Williams Nimi <williamson.nimi@gmail.com>
2022-12-09 05:45:44 -0800

    Alphabet soup without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- This is the main function
+ *
+ * Description: This function prints alphabets except e and q
+ * Return: it returns 0
+ */
+int main(void)
+{
+	char = ch;
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
