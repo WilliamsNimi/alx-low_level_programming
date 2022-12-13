3118d90a57106e787f25e745a6cb0eb9fc4c2459
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-13 14:42:39 +0100

    Deleted the main function in both files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * main- This is the main function
- *
- * Description: This function prints alphabets a-z using _putchar
- * Return: returns 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+
 /**
  * print_alphabet- This is the main function
  *
