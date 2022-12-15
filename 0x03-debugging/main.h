8dc45fffb826ab10008a85e3f13a6ba5d297537d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-15 09:18:30 +0100

    Updated main.h to include the proper header headings

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,8 @@
-#include <unistd.h>
-int largest_number(int, int, int);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+#endif /* MAIN_H */
