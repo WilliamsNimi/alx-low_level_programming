42e2bf015a01c7c1e34781bdd527143739fe0ba2
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-15 12:01:03 +0100

    prints 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers- This function prints a stream of numbers 10 times
+ *
+ * Description: It prints a stream of numbers from 0 to 14 ten times
+ * Return: It doesn't return anything
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0, i < 10; i ++)
+	{
+		for (j = 0, j < 15; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
