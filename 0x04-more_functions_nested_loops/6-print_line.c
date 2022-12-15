6eabce8f0f8b136c9904d25ad8d98bd6f1b81d3b
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-15 12:08:15 +0100

    We are printing straight lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line- This function prints a straight line
+ *
+ * Description: This function prints dashes based on user input
+ * @n: The number of times the dash should be printed
+ * Return: It has no return value. It is a void function
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+}
