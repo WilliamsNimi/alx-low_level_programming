cb56b045d7c70d3b2a138c412a134728c563c547
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-15 11:47:24 +0100

    Create a number that does not print 2 and 4.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers- This function prints numbers from 0 to 9
+ *
+ * Description: This function prints numbers from 0 to 9 except 2 and 4
+ * Return: this function doesn't return anything.
+ */
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 || i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c~
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers- This function prints numbers from 0 to 9
+ *
+ * Description: This function prints numbers from 0 to 9
+ * Return: this function doesn't return anything.
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
