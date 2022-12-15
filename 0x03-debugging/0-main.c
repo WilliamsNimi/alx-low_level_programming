c3eb2b0c78b735a33e3209a446cc9c963ade9be7
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-15 20:02:43 +0100

    Removed a trailing space

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main- this is the main function
  *
- * Description: This funciton checks that a function prints 
+ * Description: This funciton checks that a function prints
  * the right answer when given a function of 0
  * Return: It returns 0
  */
