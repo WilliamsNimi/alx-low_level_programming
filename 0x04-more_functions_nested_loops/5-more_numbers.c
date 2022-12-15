204ab4bde32e7c73c31fe4eb7be41084538f9a20
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-15 20:42:26 +0100

    fixed the logic to print characters beyond 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0, i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0, j < 15; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
