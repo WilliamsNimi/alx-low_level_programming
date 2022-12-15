74e2e9909b5c53d82ad44110ec1af3f302dd0973
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-15 23:09:31 +0100

    Finished the triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,11 +3,12 @@
  * print_triangle- This is the print triangle function
  *
  * Description: This function prints triangles the size of the input
+ * @size: The size of the pyramid
  * Return: It returns nothing. It is the void function
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < size; i++)
 	{
@@ -15,9 +16,12 @@ void print_triangle(int size)
 		{
 			_putchar(' ');
 		}
-		for (k = i; k < size; k++)
+		for (k = 0; k < size; k++)
 		{
 			_putchar('#');
 		}
+		_putchar('\n');
 	}
+	if (size <= 0)
+		_putchar('\n');
 }
