0e8824ec9a76a5a1e30c7d9bf01a37601038c197
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-19 17:25:21 +0100

    Refactored the whole code and added 2 new loops to find the length of the string and reverse the position of the pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,24 @@
 void print_rev(char *s)
 {
 	int count = 0;
+	int count2 = 0;
+	int i = 0;
 
 	while (*s != '\0')
 	{
 		count++;
 		s++;
 	}
-	while (*s != '\0')
+	count2 = count;
+	while (count2 > 0)
 	{
-		_putchar(*s + count);
-		s++;
-		count--;
+		--s;
+		count2--;
 	}
+	while (i <= count)
+	{
+		_putchar(*(s + count));
+		count = count - 1;
+	}
+	_putchar('\n');
 }
