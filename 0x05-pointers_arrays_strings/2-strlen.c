6781f66aae57c380ecbc8d3dfb71edd6483833c2
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-19 14:10:29 +0100

    Created a program to count and return the length of a string. Updated main to reflect the function prototype

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen- This is the strlen function
+ *
+ * Description: This function prints the length of a string
+ * @s: This is the value of the string to be checked
+ * Return: THis function returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int strlen;
+
+	strlen = 0;
+	while (*s[strlen] != '\0')
+	{
+		strlen++;
+	}
+	return (strlen);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@
 int _putchar(char __c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
