d7340fe058585e846d35dbb32424eda6018f7213
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-22 20:27:32 +0100

    This function concats 2 strings up to a specififed number of bytes dictated by the second string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncat- this is the _strcat function
+ *
+ * Description: This function concatenates 2 strings
+ * @dest: base string
+ * @src: String to be added to base string
+ * @n: Number of bytes to be copied
+ * Return: This function returns a string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int revcount = 0;
+	int i;
+
+	while (*dest != '\0')
+	{
+		count++;
+		dest++;
+	}
+	revcount = count;
+	while (revcount > 0)
+	{
+		dest--;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[count + i] = src[i];
+	}
+	dest[count + i] = '\0';
+	return (dest);
+}
