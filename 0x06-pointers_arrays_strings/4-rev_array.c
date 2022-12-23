6df69c985345eb107a30a9ec40d33abae1f63549
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2022-12-23 10:27:43 +0100

    Created a function to reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * reverse_array- This is the reverse array function
+ *
+ * Description: This function reverses an array
+ * @a: Array to be reversed
+ * @n: The length of the array
+ * Return: this function returns void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int beg = 0;
+	int end = n;
+	int temp = 0;
+
+	while (i < (n/2))
+	{
+		temp = a[beg];
+		a[beg] = a[end - 1];
+		a[end - 1] = temp;
+		beg++;
+		end--;
+	        i++;
+		if (beg == end)
+		{
+			a[beg] = a[beg];
+			break;
+		}
+	}
+}
