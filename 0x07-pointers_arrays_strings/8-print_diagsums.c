ef5bdeb31fe496998fc2ce59e03d0c28e1ec7643
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-02 20:02:14 +0100

    Created a faulty version of strstr

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -37,13 +37,15 @@ void print_diagsums(int *a, int size)
 	_putchar(' ');
 	_putchar(backward_sum - '0');
 }
-/*int main(void)
-{
-	int c3[3][3] = {
-		{0,1,5},
-		{10,11,12},
-		{1000,101,102},
-	};
-	print_diagsums((int *) c3, 3);
-	return (0);
-	}*/
+/**
+ *int main(void)
+ *{
+ *	int c3[3][3] = {
+ *		{0,1,5},
+ *		{10,11,12},
+ *		{1000,101,102},
+ *	};
+ *	print_diagsums((int *) c3, 3);
+ *	return (0);
+ *}
+*/
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
+char *_strstr(char *haystack, char *needle);
