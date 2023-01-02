2438bc0e1b101fab2ad14eed8bcaf41f5697c6c5
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-02 08:21:45 +0100

    Corrected the return statement for 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,9 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = 0; i < n && s[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-	return (s);
+	return (*s);
 }
