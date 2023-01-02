aa1db6ede5a93249e840467c622337d238712905
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-02 18:06:49 +0100

    Printing chess boards correctly

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strspn- This is the strspn function
  *
@@ -11,15 +12,27 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j = 0;
-	int count = 0;
+	unsigned int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == accept[j])
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			j++;
-			count++;
+			if (s[i] == accept[j])
+			{
+				count++;
+			}
 		}
 	}
 	return (count);
+}
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "oleh";
+	unsigned int n;
+
+	n = _strspn(s, f);
+	printf("%u\n", n);
+	return (0);
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,7 +14,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			putchar(a[i][j]);
 		}
+		putchar('\n');
 	}
 }
