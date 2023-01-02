dc5449b624a057b511f516660f7059902f4e8b36
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-02 17:40:47 +0100

    Fixed errors in main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
-#include <unistd.io>
+#include <unistd.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(chat *s, char c);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
