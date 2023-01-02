2ca5852c3f69f2b667bd4d0e9378e78a497b263c
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-02 15:44:39 +0100

    We are printing chessboards now

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard- This is the print_chessboard function
+ *
+ * Description: This function prints an 8x8 chessboard
+ * @a: Takes an array of 8 characters
+ * Return: Returns nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(chat *s, char c);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
