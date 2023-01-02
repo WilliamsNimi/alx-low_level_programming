2b00cafe660b7dfc293e120f2943bc11909e32a1
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-02 16:03:31 +0100

    Function that prints the sum of numbers in the diagonals of a square matrix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(chat *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
