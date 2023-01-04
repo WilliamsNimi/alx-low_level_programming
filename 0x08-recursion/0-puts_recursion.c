8dde503bc730d4af4e3bc9f8dc812a72765bf3e2
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-04 13:50:59 +0100

    created main.h. Created a function that prints a string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion- This is the _puts_recursion function
+ *
+ * Description: This function prints characters on the screen recursively
+ * @s: The string to be printed
+ * Return: Returns nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	s = s + 1;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,3 @@
+#include <unistd.h>
+int _putchar(char c);
+void _puts_recursion(char *s);
