f4b6ad8049aec8240ce6dc09226dd157092a9550
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-05 08:22:29 +0100

    Created a function to check for prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_prime_number- This is the is_prime_number function
+ *
+ * Description: This function checks if a number of a prime number
+ * @n: The number to be checked is taken as a parameter
+ * Return: It returns 1 if true, 0 if false.
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0);
+	{
+		return (0);
+	}
+	return (1);
+	is_prime_number(n/2);
+}
