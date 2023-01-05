155acdd10db97a804c679c84c149eddc5d469c86
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-05 08:28:55 +0100

    Had a semicolon in the wrong place

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0);
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
 	{
 		return (0);
 	}
