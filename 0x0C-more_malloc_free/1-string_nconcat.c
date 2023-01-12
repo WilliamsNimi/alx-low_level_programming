23439f5cfc16f6adbde8fa237983dda9b4d61ea8
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-12 14:41:54 +0100

    removed the assignment to empty string if s1 is NULL or s2 is NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *s3;
 
-	if (s1 == NULL)
-		s1[0] = '\0';
-	if (s2 == NULL)
-		s2[0] = '\0';
 	if (n >= strlen(s2))
 		n = strlen(s2);
 
