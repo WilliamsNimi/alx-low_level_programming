8200e7413d0f9d6ffaca6489f8501e8ace7d058f
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-16 19:19:44 +0100

    Created a function to create and initialize a dog object

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog: A fuction of type dog_t
+ * Description: This function creates a new dog
+ * @name: The name of the new dog
+ * @age: The age of the new dog
+ * @owner: The owner of the new dog
+ * Return: reurns a dog object
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *dog;
+
+	dog = malloc(sizeof(struct dog));
+	if (dog == NULL)
+		return NULL;
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
