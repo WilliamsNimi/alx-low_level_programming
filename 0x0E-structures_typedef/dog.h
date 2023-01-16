c73256fbb8d3560bcd647eb804023fa4e394c595
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-16 18:52:21 +0100

    added a new type fof the dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,11 @@
  *
  * Description: This struct defines a dog with a name, age and owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
