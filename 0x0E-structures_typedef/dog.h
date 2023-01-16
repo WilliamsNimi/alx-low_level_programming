545356c3819cd3b14a43e7f7ed7f162c50d9da23
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-16 18:15:17 +0100

    Created a struct for a dog object

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog.h
+#include <stdio.h>
+/**
+ * struct dog - This is a dog struct
+ * @name: Every dog must have a name
+ * @age: Every dog must have an age
+ * @owner: Every dog must have an owner
+ *
+ * Description: This struct defines a dog with a name, age and owner
+ */
+typedef struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+#endif
