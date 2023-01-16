4f3017bd7b8e88de57d6a3bdd025adcf1134edc9
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-17 00:12:44 +0100

    I added guards

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef dog
+#define dog
 #include <stdio.h>
 /**
  * struct dog - This is a dog struct
@@ -17,3 +19,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -3,11 +3,10 @@
 #include "dog.h"
 int main(void)
 {
-	struct dog my_dog;
+	dog_t *my_dog;
 
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	printf("My name is %s, and I am %.1f :) - Woff!\n", my_dog.name, my_dog.age);
+	my_dog = new_dog("poppy", 3.5, "Bob");
+	printf("%s\n%.1f\n%s",my_dog->name, my_dog->age, my_dog->owner);
+	free_dog(my_dog);
 	return (0);
 }
