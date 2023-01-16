ef1b2238df3139987c0edac08c36b8a033f36b30
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-16 23:37:26 +0100

    Got rid of unnecessary semi-colons

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
 int main(void)
 {
 	int s;
 
 	s = 98 + SIZE;
 	printf("%d\n", s);
+	printf("%.3f\n", PI);
+	printf("%d\n", ABS(-3));
+	printf("%d\n", SUM(2, 3));
 	return (0);
 }
