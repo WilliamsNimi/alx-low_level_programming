b8ed4ed571788e1fb4c83baf1c62beeb96cec62e
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-17 12:58:38 +0100

    fixed issues with header guard

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,4 @@
 #ifndef dog
-#define dog
-#include <stdio.h>
 /**
  * struct dog - This is a dog struct
  * @name: Every dog must have a name
@@ -15,20 +13,8 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-#ifndef init_dog
-#define init_dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif
-#ifndef print_dog
-#define print_dog
 void print_dog(struct dog *d);
-#endif
-#ifndef new_dog
-#define new_dog
 dog_t *new_dog(char *name, float age, char *owner);
-#endif
-#ifndef free_dog
-#define free_dog
 void free_dog(dog_t *d);
-#endif
 #endif
