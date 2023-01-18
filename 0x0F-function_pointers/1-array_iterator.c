8173cddbbc0769f9cbcf1276a249edd1009013fb
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-18 22:21:15 +0100

    Created a function to iterate an action every element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator-  This is the array_iterator function
+ * Description: This function performs operations on each element of an array
+ * @array: The array to be worked on
+ * @size: The size of the array
+ * @action: A pointer to the function acting on the array
+ * Return: Returns nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
