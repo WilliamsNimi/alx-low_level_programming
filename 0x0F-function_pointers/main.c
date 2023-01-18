7c7f3b6e6d38287530d10cb759c707743aef19b1
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-18 22:44:25 +0100

    Included stdlib in functions_headers.h

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
-void print_name_as_is(char *name)
+int is_98(int elem)
 {
-	printf("Hello, my name is %s\n", name);
-}
-void print_name_uppercase(char *name)
-{
-	unsigned int i;
-
-	printf("Hello, my uppercase name is ");
-	i = 0;
-	while (name[i])
-	{
-		if (name[i] >= 'a' && name[i] <= 'z')
-		{
-			putchar(name[i] + 'A' - 'a');
-		}
-		else
-		{
-			putchar(name[i]);
-		}
-		i++;
-	}
+	return(98 == elem);
 }
 int main(void)
 {
-	print_name("Bob", print_name_as_is);
-	print_name("Bob Dylan", print_name_uppercase);
-	printf("\n");
+	int index;
+	int array[3] = {0, -98, 98};
+
+	index = int_index(array, 3, is_98);
+	printf("%d\n",index);
 	return (0);
 }
