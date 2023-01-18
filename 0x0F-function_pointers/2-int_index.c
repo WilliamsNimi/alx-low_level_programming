a7cfd473f62e0a1708bb4ea6472d96e25656b23b
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-18 22:36:15 +0100

    Created a function to compare values in an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index- This is the int_index function
+ * Description: This function prints the index at which a comparison is 0
+ * @array: An array of integers
+ * @size: size of the array
+ * @cmp: The comparison function
+ * Return: Returns an integer being an index in the array
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
