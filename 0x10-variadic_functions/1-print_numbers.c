177c6b26e38ed66b04b30476c640fb1479c0cf06
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-20 04:36:07 +0100

    Fixed betty error and code logic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i < (n-1) && separator != NULL)
+		if (i < (n - 1))
 		{
-			printf("%d%s", va_arg(list, int), separator);
+			if (separator != NULL)
+				printf("%d%s", va_arg(list, int), separator);
+			else
+				printf("%d", va_arg(list, int));
 		}
 		else
 			printf("%d\n", va_arg(list, int));
