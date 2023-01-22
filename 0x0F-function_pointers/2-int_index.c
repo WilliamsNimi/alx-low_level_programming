1cdcde51fad099f106a00128777851475c351259
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-22 02:05:37 +0100

    Fixed null issues in in_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array != NULL && cmp != NULL)
 	{
-		return (-1);
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
+	else
+		exit(98);
 	return (-1);
 }
