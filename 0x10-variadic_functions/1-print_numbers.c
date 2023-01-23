ca7ddfac3709c6f2b02ec5c9d47455d357861e2d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-23 19:17:42 +0100

    print new line if n <= 0 in 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
+	if (n <= 0)
+		printf("\n");
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
