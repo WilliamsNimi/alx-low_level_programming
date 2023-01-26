9d3ff5bf86c6c938e62991f7c500152de54bdb9d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-26 21:21:17 +0100

    Created a function to return the number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len- This is the list_len function
+ * Description: This function returns the number of elements of a linkedlist
+ * @h: The linkedlist whose elements are to be printed
+ * Return: Returns the number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *currentNode;
+	size_t nodes;
+
+	currentNode = malloc(sizeof(list_t));
+	currentNode = h;
+	while (currentNode != NULL)
+	{
+		nodes = nodes + 1;
+		currentNode = currentNode->next;
+	}
+	return (nodes);
+}
