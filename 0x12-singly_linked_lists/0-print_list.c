d128ad1dcb42777b507f2491911b6c6c300acf1d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-27 06:41:47 +0100

    Fixed size_t issue on 0-print_list.c and 1-list_len.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,13 @@ size_t print_list(const list_t *h)
 	const list_t *currentNode;
 	size_t nodes;
 
+	nodes = 0;
 	currentNode = malloc(sizeof(list_t));
 	currentNode = h;
 	while (currentNode != NULL)
 	{
-		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
+		if (currentNode->str != NULL)
+			printf("[%d] %s\n", currentNode->len, currentNode->str);
 		else
 			printf("[0] (nil)\n");
 		nodes = nodes + 1;
