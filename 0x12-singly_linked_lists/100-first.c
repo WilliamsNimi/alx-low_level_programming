13a0ce59aa85e86cec910415f6d60ceedd2f55fd
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-01-27 22:50:43 +0100

    Playing around with advanced projects

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct data{
+	void data()
+	{
+		printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+		       }
+} data_t;
