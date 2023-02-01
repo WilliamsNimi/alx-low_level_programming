87d4b9815427c71935c9cddef6863d47c2ee4ff7
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-02-01 20:48:24 +0100

    fixed check for malloc in 3-add_nodeint_end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,6 +20,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		free(newNode);
 		return (NULL);
 	}
+	if (temp == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
 	if (*head == NULL)
 	{
 		*head = newNode;
