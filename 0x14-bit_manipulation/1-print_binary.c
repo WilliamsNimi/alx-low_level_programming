38def1dbc29c9799881619576b2304d0118678ae
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-02-03 21:27:44 +0100

    trying to print binary using bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,16 +9,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	int size_of_n;
-	unsigned int counter;
-	unsigned long mask;
+	unsigned long int size_of_n;
+	unsigned long int counter;
+	unsigned long int mask;
 
 	size_of_n = 32;
 	mask = 1 << (size_of_n - 1);
 
 	for (counter = 1; counter <= size_of_n; counter++)
 	{
-		putchar(n & mask ? '1' : '0');
-		value <<= 1;
+		_putchar(n & mask ? '1' : '0');
+		n <<= 1;
 	}
 }
