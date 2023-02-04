33ec06e0b7a79cc73f78d87820444fcfe2b8850d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-02-04 01:13:02 +0100

    Replacing bits with 0

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * set_bit- This is the set_bit function
+ * Description: This function sets bit at an index
+ * @n: the value to be changed to binary
+ * @index: the index at which a bit is to be set
+ * Return: Returns 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int val = n;
+	int mask = 1 << val;
+	if ((val & ~mask) | (0 << bit))
+		return (1);
+	return (-1);
+}
