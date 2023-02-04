1f3da69866b40610b2e04d64d4ea3f6d7940add8
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-02-04 01:15:14 +0100

    Replacing bits with 1 and 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * set_bit- This is the set_bit function
+ * clear_bit- This is the set_bit function
  * Description: This function sets bit at an index
  * @n: the value to be changed to binary
  * @index: the index at which a bit is to be set
  * Return: Returns 1 or -1
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	int val = n;
 	int mask = 1 << val;
-	if ((val & ~mask) | (1 << bit))
+	if ((val & mask) | (0 << bit))
 		return (1);
 	return (-1);
 }
