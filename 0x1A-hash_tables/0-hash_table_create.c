245ac9d1921843f49f85ffe1d31cf50514c695a8
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-04-09 19:26:36 +0100

    Created a function to create hashtables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create- This is the hash_table_create function
+ * Description: This function creates a hash table
+ * @size: The size of the hashtable array
+ * Return: Returns a pointer to the hashtable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t) * size);
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
+	return (hash_table);
+}
