483a20ddecaba840702f7fe364a3198ef8c0e414
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-04-12 22:04:05 +0100

    Freed more allocated memory in hash_node_t

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,6 +10,7 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned int i;
+	hash_node_t *curr_node, *tmp;
 
 	if (ht == NULL)
 	{
@@ -19,8 +20,19 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		for (i = 0; i < ht->size; i++)
 		{
+			curr_node  = ht->array[i];
+			while (curr_node != NULL)
+			{
+				tmp = curr_node->next;
+				free(curr_node->key);
+				free(curr_node->value);
+				free(curr_node);
+				curr_node = tmp;
+			}
+			free(tmp);
 			free(ht->array[i]);
 		}
+		free(ht->array);
 		free(ht);
 	}
 }
