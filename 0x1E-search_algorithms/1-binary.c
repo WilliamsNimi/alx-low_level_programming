50ecf0e4ced2cb67fb757bf326e88eb0d469321d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-12-12 09:58:39 +0100

    Created binary search in C

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "search_algos.h"
+#include <stdlib.h>
+/**
+ * binary_search -  The linear search algorithm
+ * Desc: Implementation of the linear search algorithm
+ * @array: The array to be searched
+ * @size: The size of the array to be searched
+ * @value: THe value to be found
+ * Return: Returns the index of the value to be found or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t mid = size / 2;
+	size_t j = 0;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	printf("Searching in array: ");
+	while (j < size)
+	{
+		printf("%d", array[j]);
+		if (j != (size - 1))
+		{
+			printf(", ");
+		}
+		j++;
+	}
+	printf("\n");
+	while (i != mid)
+	{
+		printf("Searching in array: ");
+		j = i;
+		while (j < (mid - 1))
+		{
+			printf("%d", array[j]);
+			if (j < (mid - 2))
+			{
+				printf(", ");
+			}
+			j++;
+		}
+		printf("\n");
+		if (value == array[mid])
+		{
+			return (mid);
+		}
+		else if (value > array[mid])
+		{
+			i = mid;
+			mid = (i + mid) / 2;
+		}
+		else if (value < array[mid])
+		{
+			mid = (i + mid) / 2;
+		}
+	}
+	return (-1);
+}
